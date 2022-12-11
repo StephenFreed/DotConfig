16292739d319659f416efb6bfc2a7bed94a60172
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-12-10 21:12:11 -0500

    Sat Dec 10 21:12:11 EST 2022: Auto Git Push From Laptop

---
+++ push_to_github.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DATE=$(date +"%D-%T-%Z")
+
+STATUS=$(git status -s 2> /dev/null)
+CHANGES=$(($(echo $STATUS | grep " M" | wc -l | xargs) + $(echo $STATUS | grep "M " | wc -l | xargs) + $(echo $STATUS | grep " R" | wc -l | xargs) + $(echo $STATUS | grep "R " | wc -l | xargs) + $(echo $STATUS | grep " D" | wc -l | xargs) + $(echo $STATUS | grep "D " | wc -l | xargs) + $(echo $STATUS | grep "?? " | wc -l | xargs)))
+
+if [[ $CHANGES > 0 ]]
+then
+    git add -A
+    git commit -m "$(date): Auto Git Push From Laptop"
+    git push
+    echo "$DATE PUSHED CHANGES" >> ./log_file.txt
+else
+    echo "$DATE NO CHANGES" >> ./log_file.txt
+fi
+
