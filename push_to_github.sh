28122a128083035bca0bfa570dc150cdd51f3a1b
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-12-23 14:29:34 -0500

    Fri Dec 23 14:29:34 EST 2022: Auto Git Push From Laptop

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DATE=$(date +"%D-%T-%Z")
-
 STATUS=$(git status -s 2> /dev/null)
 CHANGES=$(($(echo $STATUS | grep " M" | wc -l | xargs) + $(echo $STATUS | grep "M " | wc -l | xargs) + $(echo $STATUS | grep " R" | wc -l | xargs) + $(echo $STATUS | grep "R " | wc -l | xargs) + $(echo $STATUS | grep " D" | wc -l | xargs) + $(echo $STATUS | grep "D " | wc -l | xargs) + $(echo $STATUS | grep "?? " | wc -l | xargs)))
-echo $CHANGES
+IS_AHEAD=$(echo -n "$STATUS" | grep -i "ahead")
+echo $IS_AHEAD
+
 
 if [[ $CHANGES > 0 ]]
 then
